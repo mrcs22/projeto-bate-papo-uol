eafa39b4837eced20d021e81c8d29aaa2b30238d
mrcs22 <marcusmoraes2010@hotmail.com>
2021-04-13 09:33:08 -0300

    Modificando função hideSideMenu()

--- js/script.js
+++ js/script.js
@@ -10,8 +10,10 @@ function hideSideMenu() {
   const sidebar = document.querySelector(".sidebar");
   const cover = document.querySelector(".cover");
 
-  cover.classList.add("ocult");
   sidebar.classList.remove("show");
+  setTimeout(() => {
+    cover.classList.add("ocult");
+  }, 400);
 }
 
 function selectMenuItem(item) {
