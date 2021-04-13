1f2293b2e731f00ec3f663e7c73d2f7435e74661
mrcs22 <marcusmoraes2010@hotmail.com>
2021-04-13 02:00:59 -0300

    Finalizando função showSideMenu()

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function showSideMenu() {
+  const sidebar = document.querySelector(".sidebar");
+  const cover = document.querySelector(".cover");
+
+  cover.classList.remove("ocult");
+  sidebar.classList.add("show");
+}
