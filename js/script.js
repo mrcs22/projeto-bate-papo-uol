e2bf36a538622966c1fa1787f21cd2d60565fbe9
mrcs22 <marcusmoraes2010@hotmail.com>
2021-04-13 02:24:27 -0300

    Finalizando função selectMenuItem()

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ function showSideMenu() {
   cover.classList.remove("ocult");
   sidebar.classList.add("show");
 }
+
+function hideSideMenu() {
+  const sidebar = document.querySelector(".sidebar");
+  const cover = document.querySelector(".cover");
+
+  cover.classList.add("ocult");
+  sidebar.classList.remove("show");
+}
+
+function selectMenuItem(item) {
+  let parentElement = item.parentElement;
+  let lastSelectedItem = parentElement.querySelector(".selected");
+
+  lastSelectedItem.classList.remove("selected");
+  item.classList.add("selected");
+}
