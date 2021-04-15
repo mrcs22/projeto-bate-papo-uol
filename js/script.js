292a48eb13fe017d579cbfe73d29d96ec07db4b1
mrcs22 <marcusmoraes2010@hotmail.com>
2021-04-15 00:52:40 -0300

    Removendo funções de renderização de telas

--- js/script.js
+++ js/script.js
@@ -1,46 +1,5 @@
 let whoami = null;
 
-start();
-
-function start() {
-  renderLoginPage();
-}
-
-function renderLoginPage() {
-  const body = document.querySelector("body");
-  const loginPageContainer = document.createElement("div");
-  loginPageContainer.classList.add("login");
-
-  const div = document.createElement("div");
-
-  const logo = document.createElement("img");
-  logo.setAttribute("src", "./img/logo.svg");
-  logo.setAttribute("alt", "Bate papo uol");
-
-  const innerDiv = document.createElement("innerDiv");
-  innerDiv.classList.add("innerDiv");
-
-  const input = document.createElement("input");
-  input.setAttribute("type", "text");
-  input.setAttribute("id", "userName");
-  input.setAttribute("placeholder", "Digite seu nome");
-
-  const button = document.createElement("input");
-  button.setAttribute("type", "button");
-  button.setAttribute("value", "Entrar");
-  button.setAttribute("onclick", `tryLogin(this.previousSibling.value)`);
-
-  div.appendChild(logo);
-
-  innerDiv.appendChild(input);
-  innerDiv.appendChild(button);
-
-  loginPageContainer.appendChild(div);
-  loginPageContainer.appendChild(innerDiv);
-
-  body.appendChild(loginPageContainer);
-}
-
 function tryLogin(userName) {
   whoami = userName;
 
@@ -49,21 +8,22 @@ function tryLogin(userName) {
   };
 
   const response = axios.post(
-    "https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants ",
+    "https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants",
     user
   );
 
   response.then(() => {
     const loginPage = document.querySelector(".login");
+    const chatPage = document.querySelector(".chat");
     loginPage.classList.add("ocult");
+    chatPage.classList.remove("ocult");
+
     whoami = userName;
     openChat();
   });
 }
 
 function openChat() {
-  renderChat();
-
   renderOnlineUsers();
 
   renderMessages();
@@ -124,7 +84,7 @@ async function renderMessages() {
     "https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages"
   );
 
-  const messages = response.data.slice(80, 99);
+  const messages = response.data.slice(80, 100);
 
   populateChat(messages);
   scrollPage();
@@ -244,12 +204,12 @@ function sendMessage() {
     "https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages",
     message
   );
-
+  input.value = "";
   response.then(() => {
-    input.value = "";
+    renderMessages();
   });
 
-  response.catch((err) => window.location.reload());
+  response.catch(() => window.location.reload());
 }
 
 function scrollPage() {
@@ -258,91 +218,20 @@ function scrollPage() {
   contentDiv.scrollTo(0, contentDiv.scrollHeight);
 }
 
-function renderChat() {
-  const body = document.querySelector("body");
-
-  const htmlElements = [makeHeader(), makeContent(), makeFooter(), makeCover()];
-
-  htmlElements.forEach((element) => body.appendChild(element));
-}
-
-function makeHeader() {
-  const div = document.createElement("div");
-  div.classList.add("header");
-
-  const logo = document.createElement("img");
-  logo.setAttribute("src", "./img/logo.svg");
-  logo.setAttribute("alt", "Bate papo uol");
-
-  const headerIcon = document.createElement("ion-icon");
-  headerIcon.setAttribute("name", "people");
-  headerIcon.setAttribute("onclick", "showSideMenu()");
-
-  div.appendChild(logo);
-  div.appendChild(headerIcon);
-
-  return div;
-}
-
-function makeContent() {
-  const div = document.createElement("div");
-  div.classList.add("content");
-
-  const ul = document.createElement("ul");
-
-  div.appendChild(ul);
-
-  return div;
-}
-
-function makeFooter() {
-  const footer = document.createElement("div");
-  footer.classList.add("footer");
-
-  const div = document.createElement("div");
-
-  const input = document.createElement("input");
-  input.setAttribute("type", "text");
-  input.setAttribute("placeholder", "Escreva aqui...");
-
-  const p = document.createElement("p");
-  p.innerHTML = "Enviando para <span>Todos</span> (<span>Público</span>)";
-
-  const footerIcon = document.createElement("ion-icon");
-  footerIcon.setAttribute("name", "paper-plane-outline");
-  footerIcon.setAttribute("onclick", "sendMessage()");
-
-  div.appendChild(input);
-  div.appendChild(p);
-
-  footer.appendChild(div);
-  footer.appendChild(footerIcon);
-
-  return footer;
-}
-
-function makeCover() {
-  const cover = document.createElement("div");
-  cover.classList.add("cover", "ocult");
-  cover.setAttribute("onclick", "hideSideMenu()");
-
-  return cover;
-}
-
 function messagesLive() {
-  const interval = setInterval(async () => {
+  setInterval(async () => {
     await renderMessages();
   }, 3000);
 }
 
 function usersListLive() {
-  const interval = setInterval(async () => {
+  setInterval(async () => {
     await renderOnlineUsers();
   }, 10000);
 }
 
 function sayImOnline() {
-  const interval = setInterval(async () => {
+  setInterval(async () => {
     const user = {
       name: whoami,
     };
